2e3bc9f698c5013e36821f623b3604b8a53f0e31
Vikas Gautam <vikasgautam@Vikass-MacBook-Air.local>
2014-07-28 00:45:32 +0530

    Reverse words in a string

---
+++ Reverse words in a string.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String reverseWords(String s) {
+        s=s.trim();
+        String[] arr=s.split(" ");
+        
+        StringBuilder sb=new StringBuilder("");
+        
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            String str=arr[i].trim();
+                sb.append(str);
+                if(i!=0){
+                    sb.append(" ");
+                }
+        }
+        
+        return sb.toString().replaceAll("\\s+"," ");
+    }
+}
